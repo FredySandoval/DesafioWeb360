357aa5a47c401e72150c9e070ae72556cd65d275
Fredy Sandoval <shockerovip@gmail.com>
2024-12-23 01:46:51 -0600

    update/createUser-fix

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,18 +1,9 @@
 import { Request, Response } from 'express';
 import { Users } from '../models/Users'
-import { Op } from 'sequelize';
+import { Op, QueryTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
+import sequelize from '../database/databaseConnection';
 
-export const UserStatus = {
-  active:    1,
-  pending:   2,
-  suspended: 3,
-  deleted:   4,
-} as const;
-export const UserRoles = {
-  client:   1,
-  operator: 2,
-  admin:    3,
-} as const;
 type UserQueryParams = {
   status?: string;
   role?  : string; 
@@ -20,14 +11,25 @@ type UserQueryParams = {
   limit? : number;
   email? : string;
 }
+export enum Status {
+  active = 1,
+  pending,
+  suspended,
+  deleted,
+}
+export enum Roles {
+  client = 1,
+  operator,
+  admin,
+}
 const DEFAULT_PAGE_SIZE = 10 as const;
 const DEFAULT_PAGE = 1 as const;
 
 const getUsers = async (req:Request, res:Response) => {
   const { status , role, page, limit, email } = req.query as UserQueryParams;
   const whereCondition = {};
-  if ( status ) whereCondition.StatusID = UserStatus[status];
-  if ( role   ) whereCondition.RoleID   = UserRoles[role]
+  if ( status ) whereCondition.StatusID = Status[status];
+  if ( role   ) whereCondition.RoleID   = Roles[role]
   if ( email  ) whereCondition.email    = { [ Op.like ]: `%${email}%`} 
 
  
@@ -43,4 +45,37 @@ const getUsers = async (req:Request, res:Response) => {
   });
   return res.status(200).json(activeUsers);
 }
-export { getUsers };
+
+const createUser = async (req:Request, res:Response) => {
+    const { 
+        firstName,
+        lastName,
+        email,
+        password,
+        dateOfBirth,
+        profilePicture,
+        status,
+        role,
+    } = req.body;
+    
+    const passwordHash = await bcrypt.hash(password, 10)
+    const userData = {
+        FirstName:      firstName,
+        LastName:       lastName,
+        Email:          email,
+        DateOfBirth:    dateOfBirth, //'1990-01-01T00:00:00Z'
+        PasswordHash:   passwordHash,  // Ensure the password is hashed
+        ProfilePicture: profilePicture,
+        StatusID:       Status[status],
+        RoleID:         Roles[role]
+    };
+    const userJson = JSON.stringify(userData);
+    try {
+        const result = await sequelize.query('EXEC CreateUser @UserJson = :userJson', {replacements:{userJson}, type: QueryTypes.RAW })
+        res.json({ result })
+    } catch (error) {
+      
+        res.status(409).json({ error: error, message: 'c/1 unable to create user'})
+    }
+}
+export { getUsers, createUser };
